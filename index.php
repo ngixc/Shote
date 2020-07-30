0f1e5fa6d90429e7251fe0a20c972eb7ebde369d
creator1871 <65519405+creator1871@users.noreply.github.com>
2020-07-30 23:30:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,107 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Auto Followers &amp; Likes Instagram - FollowerGratis.co.id</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="expires" content="Sat, 26 Jul 1997 05:00:00 GMT">
+<meta http-equiv="cache-control" content="private, must-revalidate, max-age=0">
+<meta http-equiv="pragma" content="no-cache">
+<meta name="csrf-token" content="uewJpjycQzuu9mHnDaINIyw3Wc2nGjw8dzB0DM9W">
+<meta name="description" content="Dapatkan followers dan likes untuk Instagram secara gratis dengan menggunakan situs Auto Followers Instagram Indonesia yang didukung oleh Instagram">
+<meta name="keywords" content="auto followers instagram, auto likes instagram, auto komentar instagram, auto followers, auto follower ig, penambah followers, followers, likes, instagram, auto followers, followers gratis, followers indonesia, Admin Panel, Panel Gratis, Regarstore, Tools Gratis, auto followers instagram gratis">
+<meta name="author" content="FollowerGratis.co.id">
+<meta name="robots" content="index,follow">
+<meta name="language" content="ID">
+<meta name="coverage" content="Worldwide">
+<meta name="distribution" content="Global">
+<meta name="msapplication-TileColor" content="#8d77f1">
+<meta name="theme-color" content="#ffffff">
+<meta name="google-site-verification" content="zvmq3V8qu7gCKeen691rg5Bc_TWTSa7a3NmULNTvxI4" />
+<link rel="alternate" href="https://instagram.followergratis.co.id" hreflang="id-id" />
+<link rel="shortcut icon" href="https://instagram.followergratis.co.id/images/favicon.png" type="image/x-icon">
+<link rel="icon" href="https://instagram.followergratis.co.id/images/favicon.png" type="image/x-icon">
+<link rel="stylesheet" href="https://instagram.followergratis.co.id//css/app.css?id=e1966ae468770d76db33">
+<link rel="stylesheet" href="https://instagram.followergratis.co.id//css/style.css?id=f62cdb7c16db9fa74752">
+<!--[if lt IE 9]>
+	        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+<script type="a623d3beae21b0128bb31494-text/javascript">
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            
+                ga('create', 'UA-84936033-2', 'auto');
+                ga('send', 'pageview');
+                var beamer_config = {
+                    product_id : "ivvICBEp16365"
+                };
+        </script>
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top top-nav">
+<div class="container">
+<div class="navbar-header">
+<div class="navbar-toggle collapsed menu-icon" data-toggle="collapse" data-target="#menu" aria-expanded="false" role="button">
+<div class="bar half start"></div>
+<div class="bar"></div>
+<div class="bar half end"></div>
+</div>
+<div class="logo">
+<img class="navbar-brand hidden-sm hidden-xs" width="30" height="30" src="https://instagram.followergratis.co.id/images/logo/default.png" alt="logo">
+<h3 class="navbaw">Follower<span style="color: #2980B9">Gratis</span></h3>
+</div>
+</div>
+<div class="collapse navbar-collapse" id="menu" data-hover="dropdown" data-animations="fadeInUp">
+<ul class="nav navbar-nav navbar-right">
+<li><a class="click-close" href="https://instagram.followergratis.co.id">Home</a></li>
+<li><a class="click-close" href="#about" onclick="if (!window.__cfRLUnblockHandlers) return false; window.location.replace('https://instagram.followergratis.co.id#about');" data-cf-modified-a623d3beae21b0128bb31494-="">Tentang</a></li>
+<li><a class="click-close" href="#tutorial" onclick="if (!window.__cfRLUnblockHandlers) return false; window.location.replace('https://instagram.followergratis.co.id#tutorial');" data-cf-modified-a623d3beae21b0128bb31494-="">Panduan</a></li>
+<li><a class="click-close" href="#tips" onclick="if (!window.__cfRLUnblockHandlers) return false; window.location.replace('https://instagram.followergratis.co.id#tips');" data-cf-modified-a623d3beae21b0128bb31494-="">Tips Viral</a></li>
+<li><a class="click-close" href="#testi" onclick="if (!window.__cfRLUnblockHandlers) return false; window.location.replace('https://instagram.followergratis.co.id#testi');" data-cf-modified-a623d3beae21b0128bb31494-="">Testimoni</a></li>
+<li><a class="click-close sign-up" href="https://instagram.followergratis.co.id/login"><i class="fa fa-sign-in-alt"></i> Masuk</a></li>
+</ul>
+</div>
+</div>
+</nav>
+<section class="sign-up log-in testimonials">
+<div class="container">
+<div class="row">
+<div class="sign-up-content">
+<div class="container">
+<div class="row">
+<div class="form-trial col-lg-6 col-md-6 col-sm-12 col-xs-12">
+<div class="logo">
+<h2 style="color: #17AAE4">Follower<span style="color: #2980B9">Gratis</span>.co.id</h2>
+</div>
+<form action="#" method="post">
+<input type="hidden" name="_token" value="uewJpjycQzuu9mHnDaINIyw3Wc2nGjw8dzB0DM9W"> <h3>Masuk dengan Akun Instagram Anda</h3>
+<div class="name">
+<input type="text" class="form-control" name="username" id="username" type="text" placeholder="Username" minlength="4" required="true">
+</div>
+<div class="password">
+<input type="password" class="form-control" name="password" type="password" id="password" required="true" placeholder="Kata Sandi">
+<span data-toggle="#password" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+</div>
+<div class="submit">
+<button class="form-submit btn" name="submit"><i class="fa fa-sign-in-alt"></i> Secure Login</button>
+</div>
+</form>
+<hr class="wau">
+
+<script type="a623d3beae21b0128bb31494-text/javascript" src="/js/app.js?id=924b8faed96c5a491264"></script>
+<script type="a623d3beae21b0128bb31494-text/javascript">
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    var _APP_URL = "https:\/\/instagram.followergratis.co.id"
+    </script>
+<script type="a623d3beae21b0128bb31494-text/javascript" src="https://instagram.followergratis.co.id//js/login.js?id=01b15a3a26645459805c"></script>
+<script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="a623d3beae21b0128bb31494-|49" defer=""></script></body>
+</html>
